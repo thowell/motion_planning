9d601bf368a1065a11c9af8bfb421abed337ab05
thowell <thowell@stanford.edu>
2020-11-11 12:28:36 -0800

    dpo planar quadrotor

--- examples/contact_implicit/biped_steps.jl
+++ examples/contact_implicit/biped_steps.jl
@@ -17,22 +17,37 @@ open(vis)
 # 5: calf 1 (rel. to thigh 1)
 # 6: thigh 2 (rel. to downward vertical)
 # 7: calf 2 (rel. to thigh 2)
-θ = pi / 10.0
+# θ = pi / 12.5
 # q1 = initial_configuration(model, θ) # generate initial config from θ
 # qT = copy(q1)
 # qT[1] += 1.0
 # q1[3] -= pi / 30.0
 # q1[4] += pi / 20.0
 # q1[5] -= pi / 10.0
-q1, qT = loop_configurations(model, θ)
-qT[1] += 1.0
+# q1, qT = loop_configurations(model, θ)
+# qT[1] += 1.0
+
+θ = 0.0 #pi / 7.5
+q1 = zeros(model.nq)
+q1[3] = pi
+q1[4] = θ #+ pi / 20.0
+q1[5] = -2.0 * θ
+q1[6] = θ #- pi / 20.0
+q1[7] = -2.0 * θ
+q1[2] = model.l2 * cos(θ) + model.l3 * cos(θ)
+qT = copy(q1)
+qT[1] = 1.0
+# qT[2] = 1.0
+kinematics_2(model, q1, body = :leg_1, mode = :ee)[2]
+kinematics_2(model, q1, body = :leg_2, mode = :ee)[2]
+
 visualize!(vis, model, [q1])
 
 # Horizon
-T = 21
+T = 26
 
 # Time step
-tf = 1.0
+tf = 2.5
 h = tf / (T - 1)
 
 # Bounds
@@ -41,26 +56,28 @@ h = tf / (T - 1)
 # u = (τ1..4, λ1..2, β1..4, ψ1..2, η1...4, s1)
 # ul <= u <= uu
 _uu = Inf * ones(model.m)
-_uu[model.idx_u] .= 100.0
+_uu[model.idx_u] = model.uU
 
 _ul = zeros(model.m)
-_ul[model.idx_u] .= -100.0
+_ul[model.idx_u] = model.uL
 ul, uu = control_bounds(model, T, _ul, _uu)
 
-xl, xu = state_bounds(model, T)#, x1 = [q1; q1])
+xl, xu = state_bounds(model, T,
+    x1 = [q1; q1],
+    xT = [qT; qT])
 
 # Objective
 q_ref = linear_interp(q1, qT, T)
 X0 = configuration_to_state(q_ref)
 
 # penalty on slack variable
-obj_penalty = PenaltyObjective(100.0, model.m)
+obj_penalty = PenaltyObjective(1.0e5, model.m)
 
 # quadratic tracking objective
 # Σ (x - xref)' Q (x - x_ref) + (u - u_ref)' R (u - u_ref)
 obj_control = quadratic_tracking_objective(
     [zeros(model.n, model.n) for t = 1:T],
-    [Diagonal([1.0e-1 * ones(model.nu)..., zeros(model.m - model.nu)...]) for t = 1:T-1],
+    [Diagonal([1.0 * ones(model.nu)..., zeros(model.m - model.nu)...]) for t = 1:T-1],
     [zeros(model.n) for t = 1:T],
     [zeros(model.m) for t = 1:T]
     )
@@ -72,37 +89,38 @@ obj_velocity = velocity_objective(
     model.nq,
     h = h,
     idx_angle = collect([3, 4, 5, 6, 7]))
-kinematics_1(model, q1, body = :torso, mode = :com)[2]
+
 # torso height
-l_stage_torso_h(x, u, t) = 1000.0 * (kinematics_1(model, view(x, 8:14), body = :torso, mode = :com)[2] - 0.9)^2.0
-l_terminal_torso_h(x) = 1000.0 * (kinematics_1(model, view(x, 8:14), body = :torso, mode = :com)[2] - 0.9)^2.0
+t_h = kinematics_1(model, q1, body = :torso, mode = :com)[2]
+l_stage_torso_h(x, u, t) = 1000.0 * (kinematics_1(model, view(x, 8:14), body = :torso, mode = :com)[2] - t_h)^2.0
+l_terminal_torso_h(x) = 1000.0 * (kinematics_1(model, view(x, 8:14), body = :torso, mode = :com)[2] - t_h)^2.0
 obj_torso_h = nonlinear_stage_objective(l_stage_torso_h, l_terminal_torso_h)
 
 # torso lateral
-l_stage_torso_lat(x, u, t) = (1.0 * (kinematics_1(model, view(x, 8:14), body = :torso, mode = :com)[1] - kinematics_1(model, view(X0[t], 8:14), body = :torso, mode = :com)[1])^2.0)
-l_terminal_torso_lat(x) = (1.0 * (kinematics_1(model, view(x, 8:14), body = :torso, mode = :com)[1] - kinematics_1(model, view(X0[T], 8:14), body = :torso, mode = :com)[1])^2.0)
+l_stage_torso_lat(x, u, t) = (100.0 * (kinematics_1(model, view(x, 8:14), body = :torso, mode = :com)[1] - kinematics_1(model, view(X0[t], 8:14), body = :torso, mode = :com)[1])^2.0)
+l_terminal_torso_lat(x) = (100.0 * (kinematics_1(model, view(x, 8:14), body = :torso, mode = :com)[1] - kinematics_1(model, view(X0[T], 8:14), body = :torso, mode = :com)[1])^2.0)
 obj_torso_lat = nonlinear_stage_objective(l_stage_torso_lat, l_terminal_torso_lat)
 
 # foot 1 height
-l_stage_fh1(x, u, t) = 10.0 * (kinematics_2(model, view(x, 8:14), body = :leg_1, mode = :ee)[2] - 0.05)^2.0
-l_terminal_fh1(x) = 10.0 * (kinematics_2(model, view(x, 8:14), body = :leg_1, mode = :ee)[2])^2.0
+l_stage_fh1(x, u, t) = 1.0 * (kinematics_2(model, view(x, 8:14), body = :leg_1, mode = :ee)[2] - 0.025)^2.0
+l_terminal_fh1(x) = 1.0 * (kinematics_2(model, view(x, 8:14), body = :leg_1, mode = :ee)[2])^2.0
 obj_fh1 = nonlinear_stage_objective(l_stage_fh1, l_terminal_fh1)
 
 # foot 2 height
-l_stage_fh2(x, u, t) = 10.0 * (kinematics_2(model, view(x, 8:14), body = :leg_2, mode = :ee)[2] - 0.05)^2.0
-l_terminal_fh2(x) = 10.0 * (kinematics_2(model, view(x, 8:14), body = :leg_2, mode = :ee)[2])^2.0
+l_stage_fh2(x, u, t) = 1.0 * (kinematics_2(model, view(x, 8:14), body = :leg_2, mode = :ee)[2] - 0.025)^2.0
+l_terminal_fh2(x) = 1.0 * (kinematics_2(model, view(x, 8:14), body = :leg_2, mode = :ee)[2])^2.0
 obj_fh2 = nonlinear_stage_objective(l_stage_fh2, l_terminal_fh2)
 
 # initial configuration
-function l_stage_init(x, u, t)
+function l_stage_conf(x, u, t)
     if t == 1
-        return (x - [q1; q1])' * Diagonal(1000.0 * ones(model.n)) * (x - [q1; q1])
+        return 0.0#(x - [q1; q1])' * Diagonal(1000.0 * ones(model.n)) * (x - [q1; q1])
     else
         return 0.0
     end
 end
-l_terminal_init(x) = 0.0
-obj_init = nonlinear_stage_objective(l_stage_init, l_terminal_init)
+l_terminal_conf(x) = (x - [qT; qT])' * Diagonal(1000.0 * ones(model.n)) * (x - [qT; qT])
+obj_conf = nonlinear_stage_objective(l_stage_conf, l_terminal_conf)
 
 obj = MultiObjective([obj_penalty,
                       obj_control,
@@ -110,8 +128,8 @@ obj = MultiObjective([obj_penalty,
                       obj_torso_h,
                       obj_torso_lat,
                       obj_fh1,
-                      obj_fh2,
-                      obj_init])
+                      obj_fh2])#,
+                      # obj_conf])
 
 # Constraints
 con_contact = contact_constraints(model, T)
@@ -149,5 +167,5 @@ X̄, Ū = unpack(Z̄, prob)
 # Visualize
 visualize!(vis, model, state_to_configuration(X̄), Δt = h)
 
-using Plots
-plot(hcat(Ū...)[1:4,:]', label= "", linetype = :steppost)
+# using Plots
+# plot(hcat(Ū...)[1:4,:]', label= "", linetype = :steppost)
--- src/model.jl
+++ src/model.jl
@@ -25,7 +25,63 @@ control_output(model, u) = u
 
 """
 	propagate dynamics with implicit integrator
+	- LM
 """
+
+function propagate_dynamics(model, x, u, w, h, t)
+	# L-M
+	α = 1.0
+	reg = 1.0e-8
+	y = copy(x)
+
+	res(z) = fd(model, z, x, u, w, h, t)
+	merit(z) = res(z)' * res(z)
+
+	iter = 0
+
+	while iter < 100
+		me = merit(y)
+		r = res(y)
+		∇r = ForwardDiff.jacobian(res, y)
+
+		_H = ∇r' * ∇r
+		Is = Diagonal(diag(_H))
+		H = (_H + reg * Is)
+
+		pd_iter = 0
+		while !isposdef(Hermitian(Array(H)))
+			reg *= 2.0
+			H = (_H + reg * Is)
+			pd_iter += 1
+
+			if pd_iter > 100 || reg > 1.0e12
+				@error "regularization failure"
+			end
+		end
+
+		Δy = -1.0 * H \ (∇r' * r)
+
+		ls_iter = 0
+		while merit(y + α * Δy) > me + 1.0e-4 * r' * (α * Δy)
+			α *= 0.5
+			reg = reg
+			ls_iter += 1
+
+			if ls_iter > 100 || reg > 1.0e12
+				@error "line search failure"
+			end
+		end
+
+		y .+= α * Δy
+		α = min(1.2 * α, 1.0)
+		reg = 0.5 * reg
+
+		iter += 1
+
+		norm(α * Δy, Inf) < 1.0e-6 && (return y)
+	end
+end
+
 # function propagate_dynamics(model, x, u, w, h, t)
 #     _fd(z) = fd(model, z, x, u, w, h, t)
 #
@@ -96,58 +152,4 @@ function propagate_dynamics_jacobian(model, x, u, w, h, t)
 	B = -1.0 * Dy \ ForwardDiff.jacobian(du, u)
 
 	return y, A, B
-end
-
-function propagate_dynamics(model, x, u, w, h, t)
-	# L-M
-	α = 1.0
-	reg = 1.0e-8
-	y = copy(x)
-
-	res(z) = fd(model, z, x, u, w, h, t)
-	merit(z) = res(z)' * res(z)
-
-	iter = 0
-
-	while iter < 100
-		me = merit(y)
-		r = res(y)
-		∇r = ForwardDiff.jacobian(res, y)
-
-		_H = ∇r' * ∇r
-		Is = Diagonal(diag(_H))
-		H = (_H + reg * Is)
-
-		pd_iter = 0
-		while !isposdef(Hermitian(Array(H)))
-			reg *= 2.0
-			H = (_H + reg * Is)
-			pd_iter += 1
-
-			if pd_iter > 100 || reg > 1.0e12
-				@error "regularization failure"
-			end
-		end
-
-		Δy = -1.0 * H \ (∇r' * r)
-
-		ls_iter = 0
-		while merit(y + α * Δy) > me + 1.0e-4 * r' * (α * Δy)
-			α *= 0.5
-			reg = reg
-			ls_iter += 1
-
-			if ls_iter > 100 || reg > 1.0e12
-				@error "line search failure"
-			end
-		end
-
-		y .+= α * Δy
-		α = min(1.2 * α, 1.0)
-		reg = 0.5 * reg
-
-		iter += 1
-
-		norm(α * Δy, Inf) < 1.0e-6 && (return y)
-	end
 end
