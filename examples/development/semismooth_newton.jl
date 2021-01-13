20dc8945b9f36688a52f853029b06a7ab5e3f46a
taylor <thowell@stanford.edu>
2021-01-13 09:33:14 -0800

    semismooth newton in progress

--- examples/development/semismooth_newton.jl
+++ examples/development/semismooth_newton.jl
@@ -5,42 +5,37 @@ using IterativeSolvers
 # problem setup
 p = 60
 N = 30
-x_sol = max.(0.0, randn(p))
-A1 = Diagonal(-1.0 * ones(p))
-b1 = zeros(p)
-A2 = randn(N, p)
-b2 = A2 * x_sol
+x_sol = randn(p)#max.(0.0, randn(p))
+G = rand(N, p)
+h = G * x_sol
 ν_sol = randn(N)
-λ_sol = rand(p)
-λ_sol[x_sol .> 0.0] .= 0.0
-c = -A2' * ν_sol + λ_sol
+# λ_sol = rand(p)
+# λ_sol[x_sol .> 0.0] .= 0.0
+c = -G' * ν_sol# + λ_sol
 
 "Convex.jl"
 x = Variable(p)
 prob = minimize(c' * x)
-prob.constraints += b2 - A2 * x == 0.0
-prob.constraints += b1 - A1 * x >= 0.0
+prob.constraints += h - G * x == 0.0
+# prob.constraints += x >= 0.0
 
 @time solve!(prob, ECOS.Optimizer)
 
 @show prob.status
+@show prob.constraints
 @show x.value
 # @show prob.constraints[1].dual
 # @show prob.constraints[2].dual
-A = [A1; A2; -A2]
-b = [b1; b2; -b2]
-m = 2N + p
+A = G #[G; Diagonal(-ones(p))]
+b = h #[h; zeros(p)]
+m = N# + p
 k = p + m + 1
 
 Q = Array([zeros(p, p) A' c;
-     -A zeros(m, m) b;
-     -c' -b'      0.0])
-
-Q_vec = vec(Q)
+           -A zeros(m, m) b;
+           -c' -b'      0.0])
 
 function Pκ(z)
-    z[p .+ (1:m)] = max.(0.0, z[p .+ (1:m)])
-    z[end] = max.(0.0, z[end])
     z
 end
 
@@ -61,13 +56,15 @@ function Ju(z)
 
     # JP = ForwardDiff.jacobian(Pκ, ũ[p .+ (1:m)] - v[p .+ (1:m)])
 
-    JP = zeros(m, m)
-    dif = ũ[p .+ (1:m)] - v[p .+ (1:m)]
-    for i = 1:m
-        if dif[i] >= 0.0
-            JP[i, i] = 1.0
-        end
-    end
+    # JP = zeros(m, m)
+    # dif = ũ[p .+ (1:m)] - v[p .+ (1:m)]
+    # for i = 1:m
+    #     if dif[i] >= 0.0
+    #         JP[i, i] = 1.0
+    #     end
+    # end
+
+    JP = Diagonal(ones(m))
 
     if z[k] - z[3k] >= 0.0
         ℓ = 1.0
@@ -94,19 +91,19 @@ u[end] = 1.0
 v[end] = 1.0
 
 z = [ũ; u; v]
-# z = rand(3k)
+z = rand(3k)
 F(z)
 rank(Ju(z))
 J(z)
 rank(ForwardDiff.jacobian(F, z))
 norm(J(z) - ForwardDiff.jacobian(F, z), Inf)
 
-(J(z)' * J(z)) \ (J(z)' * F(z))
-Δ = zero(z)
+# (J(z)' * J(z)) \ (J(z)' * F(z))
+# Δ = zero(z)
 # Δ[k] = 1.0
 # Δ[2k] = 1.0
 # Δ[3k] = 1.0
-gmres!(Δ, J(z), -1.0 * F(z), abstol = 1.0)
+# gmres!(Δ, J(z), -1.0 * F(z), abstol = 1.0)
 
 function solve()
     ũ = zeros(k)
@@ -125,11 +122,21 @@ function solve()
         # FQ(x) = F(z, x)
 
         _F = F(z)
+        if norm(_F) < 1.0e-8
+            # return z
+            x = z[k .+ (1:p)]
+            s = z[k + p .+ (1:m)]
+            τ = z[k + k]
+            κ = z[3k]
+            println("τ = $τ")
+            println("κ = $κ")
+            return x ./ τ
+        end
         _J = J(z) #ForwardDiff.jacobian(F, z)
         # Δ = -1.0 * _J' *  inv(_J * _J') * _F
         # Δ = -1.0 * (_J' * _J) \ (_J' * _F)
         # Δ = rand(length(z))
-        gmres!(Δ, _J, -1.0 * _F, abstol = 1.0e-12, maxiter = 1000)
+        Δ = gmres(_J, -1.0 * _F, abstol = 1.0e-12, maxiter = 1000)
 
         # print(Δ)
         # @error " stop"
@@ -148,21 +155,18 @@ function solve()
         z .+= α * Δ
     end
 
-    return z
+    # return z
     x = z[k .+ (1:p)]
-    s = z[k + k .+ (1:m)]
+    s = z[k + p .+ (1:m)]
     τ = z[k + k]
+    κ = z[3k]
+    println("τ = $τ")
+    println("κ = $κ")
     return x ./ τ
 end
 
 x_sol = solve()
 
-x_sol[1:k]
-x_sol[k]
-x_sol[3k]
-F(x_sol)
-maximum(x_sol)
-G * x_sol - h
-minimum(x_sol)
 
-# norm(x_sol - x.value)
+norm(G * x_sol - h)
+x_sol
