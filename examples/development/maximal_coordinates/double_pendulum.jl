fd9ec989fc29abffd52684e94b3146a365c2e111
taylor <thowell@stanford.edu>
2021-01-15 14:43:39 -0800

    double pendulum impact

--- examples/development/maximal_coordinates/double_pendulum.jl
+++ examples/development/maximal_coordinates/double_pendulum.jl
@@ -55,8 +55,8 @@ end
 # dynamics
 function dynamics(model, q1, q2, q3, λ, h)
       nq = model.nq
-      SVector{nq}(mass_matrix(model) * (q3 - 2.0 * q2 + q1) / h
-            + h * gravity(model, q2)
+      SVector{nq}(mass_matrix(model) * (2.0 * q2 - q1 - q3) / h
+            - h * gravity(model, q2)
             + h * jacobian(model, q3)' * λ)
 end
 
@@ -198,6 +198,7 @@ plot(hcat(λ_hist...)')
 include(joinpath(pwd(), "models/visualize.jl"))
 vis = Visualizer()
 render(vis)
+
 function visualize!(vis, model, x;
         color=RGBA(0.0, 0.0, 0.0, 1.0),
         r = 0.1, Δt = 0.1)
@@ -242,57 +243,197 @@ end
 
 visualize!(vis, model, q_hist, Δt = h)
 
-# # pinned dynamics (double pendulum w/ impact)
-# function simulated_pinned_dynamics()
-#       # conditions
-#       q1 = @SVector [0.5 * model.l, 1.5, 0.5 * π, model.l + 0.5 * model.l, 1.5, 0.5 * π]
-#       q2 = copy(q1)
-#
-#       # initial force guess
-#       λ1 = zeros(4)
-#
-#       # trajectories
-#       q_hist = [q1, q2]
-#       λ_hist = []
-#
-#       for t = 1:500
-#             println("t = $t")
-#             # pinned dynamics
-#             function pinned_dynamics(x)
-#                   nq = model.nq
-#                   nλ = model.nλ
-#
-#                   q = view(x, 1:2 * nq)
-#                   λ = view(x, 2 * nq .+ (1:4))
-#
-#                   SVector{2 * nq + 4}([dynamics(model,
-#                                     view(q1, 1:nq),
-#                                     view(q2, 1:nq),
-#                                     view(q, 1:nq),
-#                                     [λ[3:4]; λ[1:2]],
-#                                     h);
-#                                    dynamics(model,
-#                                     view(q1, nq .+ (1:nq)),
-#                                     view(q2, nq .+ (1:nq)),
-#                                     view(q, nq .+ (1:nq)),
-#                                     [zeros(2); -1.0 * λ[3:4]],
-#                                     h);
-#                                    kinematics(model, view(q, 1:nq))[3:4] - [0.0; 1.5];
-#                                    kinematics(model, view(q, 1:nq))[1:2] -
-#                                     kinematics(model, view(q, nq .+ (1:nq)))[3:4]])
-#             end
-#
-#             # step
-#             x3 = newton(pinned_dynamics, [q2; zeros(4)], tol_r = 1.0e-12)
-#
-#             # cache results
-#             push!(q_hist, view(x3, 1:2 * nq))
-#             push!(λ_hist, view(x3, 2 * nq .+ (1:4)))
-#
-#             q2 = q_hist[end]
-#             q1 = q_hist[end-1]
-#             λ1 = λ_hist[end]
-#       end
-#
-#       return q_hist, λ_hist
-# end
+# pinned dynamics (double pendulum w/ impact)
+"""
+	Newton
+"""
+function newton_slack(res::Function, x;
+        idx = (1:0),
+		tol_r = 1.0e-8, tol_d = 1.0e-6)
+	y = copy(x)
+	Δy = copy(x)
+
+    r = res(y)
+
+    iter = 0
+
+    while norm(r, 2) > tol_r && iter < 25
+        ∇r = ForwardDiff.jacobian(res, y)
+
+		try
+        	Δy = -1.0 * ∇r \ r
+		catch
+			@warn "implicit-function failure"
+			return y
+		end
+
+        α = 1.0
+
+		iter_ls = 0
+
+        while any((y + α * Δy)[idx] .< 0.0)
+            α *= 0.5
+            iter_ls += 1
+            iter_ls == 50 && (@warn "line search (slack) failed")
+        end
+
+        iter_ls = 0
+
+        while α > 1.0e-8 && iter_ls < 10
+            ŷ = y + α * Δy
+            r̂ = res(ŷ)
+
+            if norm(r̂) < norm(r)
+                y = ŷ
+                r = r̂
+                break
+            else
+                α *= 0.5
+				iter_ls += 1
+            end
+
+			iter_ls == 10 && (@warn "line search failed")
+        end
+
+        iter += 1
+    end
+
+	iter == 10 && (@warn "Newton failure")
+
+    return y
+end
+
+function simulated_pinned_impact_dynamics()
+      # conditions
+      q1 = @SVector [0.5 * model.l, 1.5, 0.5 * π, model.l + 0.5 * model.l, 1.5, 0.5 * π]
+      q2 = copy(q1)
+
+      function pinned_dynamics_nom(x)
+            nq = model.nq
+            nλ = model.nλ
+
+            q = view(x, 1:2 * nq)
+            λ = view(x, 2 * nq .+ (1:4))
+
+            SVector{2 * nq + 4}([dynamics(model,
+                              view(q1, 1:nq),
+                              view(q2, 1:nq),
+                              view(q, 1:nq),
+                              [λ[3:4]; λ[1:2]],
+                              h);
+                             dynamics(model,
+                              view(q1, nq .+ (1:nq)),
+                              view(q2, nq .+ (1:nq)),
+                              view(q, nq .+ (1:nq)),
+                              [zeros(2); -1.0 * λ[3:4]],
+                              h);
+                             kinematics(model, view(q, 1:nq))[3:4] - [0.0; 1.5];
+                             kinematics(model, view(q, 1:nq))[1:2] -
+                              kinematics(model, view(q, nq .+ (1:nq)))[3:4]])
+      end
+
+      x1 = newton(pinned_dynamics_nom, [q2; zeros(4)],
+          tol_r = 1.0e-8)
+
+      # initial force guess
+      λ1 = [x1[2nq .+ (1:4)]; 0.0]
+
+      # trajectories
+      q_hist = [q1, q2]
+      λ_hist = []
+
+      for t = 1:300
+            println("t = $t")
+
+            ρ = 1.0
+            # pinned dynamics
+            function pinned_dynamics(x)
+                  nq = model.nq
+                  nλ = model.nλ
+
+                  q = view(x, 1:2 * nq)
+                  λ = view(x, 2 * nq .+ (1:5))
+                  s = x[2 * nq + 5 + 1]
+
+                  SVector{2 * nq + 6}([dynamics(model,
+                                    view(q1, 1:nq),
+                                    view(q2, 1:nq),
+                                    view(q, 1:nq),
+                                    [λ[3:4]; λ[1:2]],
+                                    h);
+                                   dynamics(model,
+                                    view(q1, nq .+ (1:nq)),
+                                    view(q2, nq .+ (1:nq)),
+                                    view(q, nq .+ (1:nq)),
+                                    [0.0; λ[5]; -1.0 * λ[3:4]],
+                                    h);
+                                   kinematics(model, view(q, 1:nq))[3:4] - [0.0; 1.5];
+                                   kinematics(model, view(q, 1:nq))[1:2] -
+                                    kinematics(model, view(q, nq .+ (1:nq)))[3:4];
+                                   s - kinematics(model, view(q, nq .+ (1:nq)))[2];
+                                   1.0 / ρ + λ[5] * s])
+            end
+
+            # step
+            x3 = newton_slack(pinned_dynamics, [q2; λ1[1:4]; 0.1; 0.1],
+                idx = (2 * nq + 4 .+ (1:2)),
+                tol_r = 1.0e-8)
+
+            for i = 1:5
+                println("   i = $i")
+
+                ρ *= 10.0
+                # pinned dynamics
+                function _pinned_dynamics(x)
+                      nq = model.nq
+                      nλ = model.nλ
+
+                      q = view(x, 1:2 * nq)
+                      λ = view(x, 2 * nq .+ (1:5))
+                      s = x[2 * nq + 6]
+
+                      SVector{2 * nq + 6}([dynamics(model,
+                                        view(q1, 1:nq),
+                                        view(q2, 1:nq),
+                                        view(q, 1:nq),
+                                        [λ[3:4]; λ[1:2]],
+                                        h);
+                                       dynamics(model,
+                                        view(q1, nq .+ (1:nq)),
+                                        view(q2, nq .+ (1:nq)),
+                                        view(q, nq .+ (1:nq)),
+                                        [0.0; λ[5]; -1.0 * λ[3:4]],
+                                        h);
+                                       kinematics(model, view(q, 1:nq))[3:4] - [0.0; 1.5];
+                                       kinematics(model, view(q, 1:nq))[1:2] -
+                                        kinematics(model, view(q, nq .+ (1:nq)))[3:4];
+                                       s - kinematics(model, view(q, nq .+ (1:nq)))[2];
+                                       1.0 / ρ - λ[5] * s])
+                end
+
+                # step
+                x3 = newton_slack(_pinned_dynamics, x3,
+                    idx = (2 * nq + 4 .+ (1:2)),
+                    tol_r = 1.0e-8)
+
+                println("COMP: $(x3[2nq + 5] * x3[2nq + 6])")
+                println("ρ: $ρ")
+                println("error: $(norm(_pinned_dynamics(x3)))")
+            end
+
+            # cache results
+            push!(q_hist, view(x3, 1:2 * nq))
+            push!(λ_hist, view(x3, 2 * nq .+ (1:5)))
+
+            q2 = q_hist[end]
+            q1 = q_hist[end-1]
+            λ1 = λ_hist[end]
+      end
+
+      return q_hist, λ_hist
+end
+
+q_hist, λ_hist = simulated_pinned_impact_dynamics()
+vis = Visualizer()
+render(vis)
+visualize!(vis, model, q_hist, Δt = h)
