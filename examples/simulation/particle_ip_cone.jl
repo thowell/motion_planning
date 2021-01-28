ea0c693fcffad40c304bf15b3dafb6bf77bcd1eb
taylor <thowell@stanford.edu>
2021-01-28 12:09:56 -0800

    trying ip cone sim

---
+++ examples/simulation/particle_ip_cone.jl
@@ -0,0 +1,328 @@
+using LinearAlgebra, ForwardDiff, StaticArrays
+using IterativeSolvers
+using Plots
+
+"""
+    second-order cone
+"""
+function κ_so(z)
+    z1 = z[2:end]
+    z0 = z[1]
+
+    z_proj = zero(z)
+    status = false
+
+    if norm(z1) <= z0
+        z_proj = copy(z)
+        # if norm(z1) < z0
+        #     status = true
+        # end
+        status = true
+    elseif norm(z1) <= -z0
+        z_proj = zero(z)
+    else
+        a = 0.5 * (1.0 + z0 / norm(z1))
+        z_proj[1:end-1] = a * z1
+        z_proj[end] = a * norm(z1)
+    end
+
+    return z_proj, status
+end
+J = [1.0 zeros(1, 2);
+     zeros(2) Diagonal(-1.0 * ones(2))]
+H_inv(u) = 2.0 * u * u' - (u' * J * u) * J
+H_inv_sq(u) = [u[1] u[2:3]';
+               u[2:3] (u[2:3] * u[2:3]') / (u[1] + (u' * J * u)^0.5) + ((u' * J * u)^0.5) * Diagonal(ones(2))]
+# ww, _ = κ_so(rand(3))
+# ww[1] += 1.0
+# hh = H_inv(ww)
+# hhsq = H_inv_sq(ww)
+# ss = sqrt(hh)
+# ss' * ss
+
+# @show x = rand(3)
+# @show x_proj, status = κ_so(x)
+# x_proj[1] += 1.0
+# x_proj
+# @show x_proj_proj, status = κ_so(x_proj)
+
+"""
+    particle dynamics
+    - 3D particle subject to contact forces
+
+    - configuration: q = (x, y, z) ∈ R³
+    - impact force (magnitude): n ∈ R₊
+    - friction force: β ∈ R⁴₊
+        - contact force: λ = (β, n) ∈ R⁴₊ × R₊
+        - friction coefficient: μ ∈ R₊
+
+    Discrete Mechanics and Variational Integrators
+        pg. 363
+"""
+struct Particle
+      m # mass
+      g # gravity
+      μ # friction coefficient
+
+      nq # configuration dimension
+end
+
+# mass matrix
+function mass_matrix(model)
+    m = model.m
+
+    Diagonal(@SVector [m, m, m])
+end
+
+# gravity
+function gravity(model, q)
+    m = model.m
+    g = model.g
+
+    @SVector [0.0, 0.0, m * g]
+end
+
+# signed distance function
+function signed_distance(model, q)
+    q[3]
+end
+
+# contact force Jacobian
+function jacobian(model, q)
+    Diagonal(@SVector ones(model.nq))
+end
+
+# dynamics
+function dynamics(model, q1, q2, q3, λ, h)
+    nq = model.nq
+    # println(q1)
+    # println(q2)
+    # println(q3)
+    # println(λ)
+    SVector{nq}(mass_matrix(model) * (2.0 * q2 - q1 - q3) / h
+        - h * gravity(model, q2)
+        + h * jacobian(model, q3)' * λ)
+end
+
+"""
+    step particle system
+        solves 1-step feasibility problem
+"""
+function _step(q1, q2, h;
+    tol = 1.0e-8, z0_scale = 0.001, step_type = :gmres, max_iter = 100)
+    # 1-step optimization problem:
+    #     find z
+    #     s.t. r(z) = 0
+    #
+    # z = (q, n, b, bs, bz, s)
+    #     s are slack variables for convenience
+
+    # initialize
+    z = 1.0e-1 * ones(14)
+    z[1:3] = copy(q2)
+
+    # initialize soc variables
+    bs, _ = κ_so(z[7:9])
+    bs[1] += 1.0
+    bz, _ = κ_so(z[10:12])
+    bz[1] += 1.0
+
+    z[7:9] = bs
+    z[10:12] = bz
+
+    ρ = 1.0 # barrier parameter
+    flag = false
+
+    for k = 1:5
+        function r(z)
+            # system variables
+            q3 = view(z, 1:3)
+            n = z[4]
+            b = view(z, 5:6)
+            bs = view(z, 7:9)
+            bz = view(z, 10:12)
+            s1 = z[13]
+            s2 = z[14]
+
+            λ = [b; n] # contact forces
+            ϕ = signed_distance(model, q3)        # signed-distance function
+            vT = (view(q3, 1:2) - view(q2, 1:2)) ./ h
+
+            G = [zeros(1, 2); -Diagonal(ones(2))]
+            g = [model.μ * n; zeros(2)]
+
+            # action optimality conditions
+            [dynamics(model, q1, q2, q3, λ, h);
+             s1 - ϕ;
+             n * s1 - ρ;
+
+             # maximum dissipation optimality conditions
+             G' * bz + vT;
+             bs + G * b - g;
+             bs' * bz - ρ;
+             bs[1] * bz[2:3] + bz[1] * bs[1:2]]
+        end
+
+        # Jacobian
+        function R(z)
+            # differentiate r
+            _R = ForwardDiff.jacobian(r, z)
+
+            return _R
+        end
+
+        function check_variables(z)
+            # system variables
+            q3 = view(z, 1:3)
+            n = z[4]
+            b = view(z, 5:6)
+            bs = view(z, 7:9)
+            bz = view(z, 10:12)
+
+            s1 = z[13]
+            s2 = z[14]
+
+            if n <= 0.0
+                return true
+            end
+
+            if !κ_so(bs)[2]
+                println("bs not in cone")
+                return true
+            end
+
+            if !κ_so(bz)[2]
+                println("bz not in cone")
+                return true
+            end
+
+            if s1 <= 0.0
+                return true
+            end
+
+            if s2 <= 0.0
+                return true
+            end
+
+            return false
+        end
+
+        extra_iters = 0
+
+        for i = 1:max_iter
+            # compute residual, residual Jacobian
+            res = r(z)
+            if norm(res) < tol
+                println("   iter ($i) - norm: $(norm(res))")
+                # return z, true
+                flag = true
+                continue
+            end
+
+            jac = R(z)
+
+            # compute step
+            # if step_type == :gmres
+            #     Δ = gmres(jac, res, abstol = 1.0e-12, maxiter = i + extra_iters)
+            # else
+            # Δ = (jac' * jac + 0.0e-6 * I) \ (jac' * res) # damped least-squares direction
+            Δ = jac \ res
+            # end
+
+            # line search the step direction
+            α = 1.0
+
+            # @show check_variables(z - α * Δ)
+            # @error "STOP"
+
+            iter = 0
+            while check_variables(z - α * Δ) # backtrack inequalities
+                α = 0.5 * α
+                # println("   α = $α")
+                iter += 1
+                if iter > 50
+                    @error "backtracking line search fail"
+                    flag = false
+                    return z, false
+                end
+            end
+
+            while norm(r(z - α * Δ))^2.0 >= (1.0 - 0.001 * α) * norm(res)^2.0
+                α = 0.5 * α
+                # println("   α = $α")
+                iter += 1
+                if iter > 50
+                    @error "line search fail"
+                    flag = false
+                    return z, false
+                end
+            end
+
+            # update
+            z .-= α * Δ
+        end
+
+        ρ = 0.1 * ρ
+        println("ρ: $ρ")
+    end
+
+    return z, flag
+end
+
+"""
+    simulate
+    - solves 1-step feasibility problem for T time steps
+    - initial configurations: q1, q2 (note this can encode initial velocity)
+    - time step: h
+"""
+function simulate(q1, q2, T, h;
+    z0_scale = 0.0, step_type = :gmres)
+    println("simulation")
+
+    # initialize histories
+    q = [q1, q2]
+    n = [0.0]
+    b = [zeros(2)]
+
+    # step
+    for t = 1:T
+        println("   t = $t")
+        z_sol, status = _step(q[end-1], q[end], h,
+            z0_scale = z0_scale, step_type = step_type)
+
+        if !status
+            @error "failed step (t = $t)"
+            return q, n, b
+        else
+            push!(q, view(z_sol, 1:3))
+            push!(n, z_sol[4])
+            push!(b, view(z_sol, 5:6))
+        end
+    end
+
+    return q, n, b
+end
+
+# simulation setup
+model = Particle(1.0, 9.81, 1.0, 3)
+h = 0.1
+
+# initial conditions
+# v1 = [1.0; 1.0; 0.0]
+# q1 = [0.0; 0.0; 1.0]
+
+v1 = [0.0; 0.0; 0.0]
+q1 = [0.0; 0.0; 1.0]
+
+v2 = v1 - gravity(model, q1) * h
+q2 = q1 + 0.5 * (v1 + v2) * h
+
+q_sol, y_sol, b_sol = simulate(q1, q2, 1, h)
+
+plot(hcat(q_sol...)[3:3, :]', xlabel = "", label = "z")
+plot!(h .* hcat(y_sol...)', xlabel = "", label = "n", linetype = :steppost)
+
+plot(hcat(q_sol...)[1:2, :]', xlabel = "", label = ["x" "y"])
+plot!(h * hcat(b_sol...)', label = ["b1" "b2"], linetype = :steppost)
+
+b_sol
