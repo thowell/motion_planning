1470ad96a359660f0c5b388fdd6bcd3f6427ccae
thowell <thowell@stanford.edu>
2021-05-15 12:25:37 -0700

    belly flop

---
+++ examples/differential_dynamic_programming/rocket3D.jl
@@ -0,0 +1,112 @@
+using Plots
+include_ddp()
+
+# Model
+include_model("rocket3D")
+
+function fd(model::Rocket3D{Midpoint, FixedTime}, x, u, w, h, t)
+	return view(x, 1:model.n) + h * f(model, view(x, 1:model.n) + 0.5 * h * f(model, view(x, 1:model.n), view(u, 1:model.m), w), view(u, 1:model.m), w)
+end
+
+n = model.n
+m = model.m
+
+# Time
+T = 201
+h = 0.01
+
+# Initial conditions, controls, disturbances
+x1 = zeros(model.n)
+x1[1] = 1.0
+x1[2] = 1.0
+x1[3] = 10.0
+mrp = MRP(RotY(-0.5 * π) * RotX(0.25 * π))
+x1[4:6] = [mrp.x; mrp.y; mrp.z]
+
+visualize!(vis, model, [x1], Δt = h)
+
+xT = zeros(model.n)
+# xT[1] = 2.5
+# xT[2] = 0.0
+xT[3] = model.length
+
+u_ref = [0.0; 0.0; 0.0]#model.mass * 9.81]
+ū = [u_ref + [1.0e-2; 1.0e-2; 1.0e-2] .* randn(model.m) for t = 1:T-1]
+w = [zeros(model.d) for t = 1:T-1]
+
+# Rollout
+x̄ = rollout(model, x1, ū, w, h, T)
+# x̄ = linear_interpolation(x1, xT, T)
+# plot(hcat(x̄...)')
+
+# Objective
+Q = [(t < T ? 1.0 * Diagonal([1.0e-3 * ones(3); 0.0 * ones(3); 1.0e-3 * ones(3); 10.0 * ones(3)])
+        : 0.0 * Diagonal(0.0 * ones(model.n))) for t = 1:T]
+q = [-2.0 * Q[t] * xT for t = 1:T]
+
+R = [Diagonal([100.0; 100.0; 1.0]) for t = 1:T-1]
+r = [-2.0 * R[t] * u_ref  for t = 1:T-1]
+
+obj = StageCosts([QuadraticCost(Q[t], q[t],
+	t < T ? R[t] : nothing, t < T ? r[t] : nothing) for t = 1:T], T)
+
+function g(obj::StageCosts, x, u, t)
+	T = obj.T
+    if t < T
+		Q = obj.cost[t].Q
+		q = obj.cost[t].q
+	    R = obj.cost[t].R
+		r = obj.cost[t].r
+        return x' * Q * x + q' * x + u' * R * u + r' * u
+    elseif t == T
+		Q = obj.cost[T].Q
+		q = obj.cost[T].q
+        return x' * Q * x + q' * x
+    else
+        return 0.0
+    end
+end
+
+# Constraints
+p = [t < T ? 2 * m : n for t = 1:T]
+info_t = Dict(:ul => [-5.0; -5.0; 0.0], :uu => [5.0; 5.0; 100.0], :inequality => (1:2 * m))
+info_T = Dict(:xT => xT)
+con_set = [StageConstraint(p[t], t < T ? info_t : info_T) for t = 1:T]
+
+function c!(c, cons::StageConstraints, x, u, t)
+	T = cons.T
+	p = cons.con[t].p
+
+	if t < T
+		ul = cons.con[t].info[:ul]
+		uu = cons.con[t].info[:uu]
+		c .= [ul - u; u - uu]
+	elseif t == T
+		xT = cons.con[T].info[:xT]
+		c .= x - xT
+	else
+		nothing
+	end
+end
+
+prob = problem_data(model, obj, con_set, copy(x̄), copy(ū), w, h, T)
+
+# Solve
+@time constrained_ddp_solve!(prob,
+    max_iter = 1000, max_al_iter = 10,
+	con_tol = 1.0e-3,
+	ρ_init = 1.0, ρ_scale = 10.0)
+
+x, u = current_trajectory(prob)
+x̄, ū = nominal_trajectory(prob)
+
+# Trajectories
+plot(hcat(ū...)', linetype = :steppost)
+plot(hcat(x̄...)[1:3, :]', linetype = :steppost)
+
+# Visualize
+include(joinpath(pwd(), "models/visualize.jl"))
+vis = Visualizer()
+render(vis)
+# open(vis)
+visualize!(vis, model, x̄, Δt = h)
---
+++ models/rocket3D.jl
@@ -0,0 +1,92 @@
+"""
+      Rocket3D
+"""
+
+struct Rocket3D{I, T} <: Model{I, T}
+      n::Int
+      m::Int
+      d::Int
+
+      mass          # mass
+      inertia       # inertia matrix
+      inertia_inv   # inertia matrix inverse
+      gravity       # gravity
+      length        # length (com to thruster)
+end
+
+function f(model::Rocket3D, z, u, w)
+      # states
+      x = view(z,1:3)
+      r = view(z,4:6)
+      v = view(z,7:9)
+      ω = view(z,10:12)
+
+      # force in body frame
+      F = view(u, 1:3)
+
+      # torque in body frame
+      τ = @SVector [model.length * u[2],
+                    -model.length * u[1],
+                    0.0]
+
+      SVector{12}([v;
+                   0.25 * ((1.0 - r' * r) * ω - 2.0 * cross(ω, r) + 2.0*(ω' * r) * r);
+                   model.gravity + (1.0 / model.mass) * MRP(r[1], r[2], r[3]) * F;
+                   model.inertia_inv * (τ - cross(ω, model.inertia * ω))])
+end
+
+n, m, d = 12, 3, 0
+
+mass = 1.0
+len = 1.0
+inertia = Diagonal(@SVector[1.0 / 12.0 * mass * len^2.0, 1.0 / 12.0 * mass * len^2.0, 1.0e-5])
+inertia_inv = Diagonal(@SVector[1.0 / (1.0 / 12.0 * mass * len^2.0), 1.0 / (1.0 / 12.0 * mass * len^2.0), 1.0 / (1.0e-5)])
+
+model = Rocket3D{Midpoint, FixedTime}(n, m, d,
+                  mass,
+                  inertia,
+                  inertia_inv,
+                  @SVector[0.0, 0.0, -9.81],
+                  len)
+
+function visualize!(vis, p::Rocket3D, q; Δt = 0.1)
+	default_background!(vis)
+
+	body = Cylinder(Point3f0(0.0, 0.0, -1.0 * model.length),
+		Point3f0(0.0, 0.0, 1.0 * model.length),
+		convert(Float32, 0.15))
+
+	setobject!(vis["rocket"], body,
+		MeshPhongMaterial(color = RGBA(0.0, 0.0, 0.0, 1.0)))
+		anim = MeshCat.Animation(convert(Int, floor(1.0 / Δt)))
+
+	for t = 1:length(q)
+	    MeshCat.atframe(anim, t) do
+	        settransform!(vis["rocket"],
+	              compose(Translation(q[t][1:3]),
+	                    LinearMap(MRP(q[t][4:6]...) * RotX(0.0))))
+	    end
+	end
+
+	# settransform!(vis["/Cameras/default"], compose(Translation(0.0, 0.0, 0.0),
+	# LinearMap(RotZ(pi/2))))
+	MeshCat.setanimation!(vis, anim)
+end
+
+# include(joinpath(pwd(), "models/visualize.jl"))
+#
+# vis = Visualizer()
+# render(vis)
+# q0 = zeros(model.n)
+#
+# # simulation test
+# u = [0.0; 0.0; 1.0]
+# T = 10
+# h = 0.1
+# x_hist = [q0]
+#
+# for t = 1:T
+# 	push!(x_hist, fd(model, x_hist[end], u, zeros(model.d), h, t))
+# end
+#
+# visualize!(vis, model, x_hist, Δt = 0.1)
